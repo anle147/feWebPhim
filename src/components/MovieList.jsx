d876f48fa3a224b8a1a9003107e518477b2468e6
anle147 <annganle@gmail.com>
2026-02-11 13:34:57 +0700

    fix layout weblist

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,30 +1,36 @@
 import { useEffect, useState } from "react";
 import api from "../services/api";
+import "./MovieList.css";
 
 function MovieList() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     api.get("/movies")
-      .then(res => {
-        console.log("MOVIES:", res.data);
-        setMovies(res.data);
-      })
+      .then(res => setMovies(res.data))
       .catch(err => console.error(err));
   }, []);
 
   return (
-    <div className="movie-list">
-      <h2>Danh sách phim</h2>
+    <div className="movie-container">
+      <h2 className="section-title">PHIM ĐỀ CỬ</h2>
 
-      {movies.map(movie => (
-        <div key={movie.MovieID} className="movie-card">
-          <h3>{movie.Title}</h3>
-          <p>{movie.Description}</p>
-          <p>Năm: {movie.ReleaseYear}</p>
-          <p>Quốc gia: {movie.Country}</p>
-        </div>
-      ))}
+      <div className="movie-grid">
+        {movies.map(movie => (
+          <div key={movie.MovieID} className="movie-card">
+            <img
+              src={movie.PosterURL}
+              alt={movie.Title}
+              className="movie-poster"
+            />
+
+            <div className="movie-info">
+              <h4>{movie.Title}</h4>
+              <p>{movie.ReleaseYear}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
